97613d60be4a66ad447a6c135415237e6cb1d958
Maciej <macglowacki@gmail.com>
2026-05-13 20:30:55 +0200

    dodano endpointy kategorii

--- routers/kategorie.py
+++ routers/kategorie.py
@@ -1,3 +1,23 @@
 from fastapi import APIRouter
+from db import conn, cursor
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/kategorie", tags=["kategorie"])
+
+
+class Kategoria(BaseModel):
+    nazwa: str
+
+
+@router.get("/")
+def pobierz_kategorie():
+    cursor.execute("SELECT * FROM kategorie")
+    kategorie = cursor.fetchall()
+    return [{"id": k[0], "nazwa": k[1]} for k in kategorie]
+
+
+@router.post("/")
+def dodaj_kategorie(kategoria: Kategoria):
+    cursor.execute("INSERT INTO kategorie (nazwa) VALUES (?)", (kategoria.nazwa,))
+    conn.commit()
+    return {"message": f"Dodano kategorie: {kategoria.nazwa}"}
